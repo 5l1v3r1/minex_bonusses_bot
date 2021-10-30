02e3750f736e1eec38ba702de8eb767296ef3f28
TurKLoJeN <32311900+TurKLoJeN@users.noreply.github.com>
2021-10-30 22:31:22 +0300

    main file

---
+++ MinexBot/Form1.cs
@@ -0,0 +1,79 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Timers;
+
+namespace MinexBot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+          
+
+            while (true)
+            {
+                string kullaniciAdi, sifre;
+
+                kullaniciAdi = textBox1.Text;
+                sifre = textBox1.Text;
+
+
+                IWebDriver driver = new ChromeDriver();
+                driver.Navigate().GoToUrl("https://minex.world/login");
+                Thread.Sleep(8000);
+
+
+                IWebElement userName = driver.FindElement(By.Name("login"));
+                IWebElement password = driver.FindElement(By.Name("password"));
+                IWebElement loginBtn = driver.FindElement(By.CssSelector(".btn.btn-base.mt-4"));
+
+                userName.SendKeys(kullaniciAdi);
+                password.SendKeys(sifre);
+                loginBtn.Click();
+                Thread.Sleep(3000);
+
+
+
+                driver.Navigate().GoToUrl("https://minex.world/cabinet/bonuses");
+                Thread.Sleep(3000);
+
+                IWebElement bonussesBtn = driver.FindElement(By.XPath("//*[@id=\"get-bonus\"]"));
+                bonussesBtn.Click();
+                Thread.Sleep(5000);
+
+
+
+                driver.Close();
+                driver.Quit();
+                Thread.Sleep(3600000);
+            }
+            
+             
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
